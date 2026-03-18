1f258785121238283de9dd4f3036ae67e8492b8c
yu-jin2 <151485371+yu-jin2@users.noreply.github.com>
2026-03-18 06:44:17 +0000

    Deploying to gh-pages from @ yu-jin2/yu-jin2.github.io@823fad5c5482a8338b173a93aa6c227ec4810441 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-paper",
           title: "Paper",
           description: "A list of my papers.",
