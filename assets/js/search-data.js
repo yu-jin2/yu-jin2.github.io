2b939d101229695090f01fe7962641af9ed39cfa
yu-jin2 <151485371+yu-jin2@users.noreply.github.com>
2026-03-18 07:27:27 +0000

    Deploying to gh-pages from @ yu-jin2/yu-jin2.github.io@e6b1b34d892e1e1bc1220d2277c2c5741aabb2c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-스테레오-카메라와-2d-lidar-통합-캘리브레이션",
+          title: '스테레오 카메라와 2D LiDAR 통합 캘리브레이션',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
