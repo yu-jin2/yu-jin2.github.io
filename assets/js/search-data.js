36445c849d4e047fe5bc1cfd6a9cbff5a0f3206f
yu-jin2 <151485371+yu-jin2@users.noreply.github.com>
2026-03-18 07:29:21 +0000

    Deploying to gh-pages from @ yu-jin2/yu-jin2.github.io@837b4b428964d01989a5eb8d6a462a6c22539026 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-종합설계-스마트폰-내-탑재된-센서를-활용한-측위-시스템",
+          title: '종합설계(스마트폰 내 탑재된 센서를 활용한 측위 시스템)',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
