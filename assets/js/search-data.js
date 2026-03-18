c3c783c71d14ec0ef3c057e667d85ae2edf45b64
yu-jin2 <151485371+yu-jin2@users.noreply.github.com>
2026-03-18 07:41:57 +0000

    Deploying to gh-pages from @ yu-jin2/yu-jin2.github.io@009070f855578f6115d306176380ef67502d0254 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/activity/";
+            window.location.href = "/activity/";
           },
         },{id: "nav-paper",
           title: "Paper",
